d65e9dbe2c3acdc9004cd02b7600b4e3da5f4cdd
rkdls <rkdls9@naver.com>
2017-06-18 23:46:03 +0900

    첫번째 커밋 테스트

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,11 +23,11 @@ class Header extends React.Component {
           <Navigation />
           <Link className={s.brand} to="/">
             <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>pikachu</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>pikachu</h1>
+            <p className={s.bannerDesc}>hello world !</p>
           </div>
         </div>
       </div>
