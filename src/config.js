9c78137b886e62ddd572ab74c9c181533c7b03fc
noeyhjun <kane7274@gmail.com>
2017-07-10 22:03:28 +0900

    불필요한 거 삭제

--- src/config.js
+++ src/config.js
@@ -40,25 +40,25 @@ module.exports = {
 
     // https://developers.facebook.com/
     facebook: {
-      id: process.env.FACEBOOK_APP_ID || '186244551745631',
-      secret: process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+      id: process.env.FACEBOOK_APP_ID,
+      secret: process.env.FACEBOOK_APP_SECRET,
     },
 
     // https://cloud.google.com/console/project
     google: {
-      id: process.env.GOOGLE_CLIENT_ID || '251410730550-ahcg0ou5mgfhl8hlui1urru7jn5s12km.apps.googleusercontent.com',
-      secret: process.env.GOOGLE_CLIENT_SECRET || 'Y8yR9yZAhm9jQ8FKAL8QIEcd',
+      id: process.env.GOOGLE_CLIENT_ID,
+      secret: process.env.GOOGLE_CLIENT_SECRET,
     },
 
     // https://apps.twitter.com/
     twitter: {
-      key: process.env.TWITTER_CONSUMER_KEY || 'Ie20AZvLJI2lQD5Dsgxgjauns',
-      secret: process.env.TWITTER_CONSUMER_SECRET || 'KTZ6cxoKnEakQCeSpZlaUCJWGAlTEBJj0y2EMkUBujA7zWSvaQ',
+      key: process.env.TWITTER_CONSUMER_KEY,
+      secret: process.env.TWITTER_CONSUMER_SECRET,
     },
 
     kakao: {
       native: process.env.KAKAO_NATIVE_APP,
-      rest: process.env.KAKAO_REST_API,
+      rest: process.env.KAKAO_REST_API, // passport로 작업할때는 REST키를 사용해야합니다.
       javascript: process.env.KAKAO_JAVASCRIPT,
       admin: process.env.KAKAO_ADMIN,
     },
