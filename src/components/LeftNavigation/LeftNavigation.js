f18a523cb3d60ea89b4ca8a0d3f53197736385df
noeyhjun <kane7274@gmail.com>
2017-07-18 23:38:48 +0900

    전체적인 레이아웃 변경

---
+++ src/components/LeftNavigation/LeftNavigation.js
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './LeftNavigation.css';
+
+class LeftNavigation extends React.Component {
+  state = {
+    thumbnail: '//s-media-cache-ak0.pinimg.com/736x/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5--pikachu-halloween-costume-diy-halloween-costumes.jpg',
+  }
+
+  componentWillMount() {
+    // ㅇ유저 정보가 store에 존재할 경우
+    if (this.props.user) { // eslint-disable-line react/prop-types
+      this.setState({
+        thumbnail: this.props.user.thumb || this.state.thumbnail, // eslint-disable-line react/prop-types, max-len
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className={s.lnb}>
+        hello
+      </div>
+    );
+  }
+}
+const mapState = state => ({
+  user: state.user,
+});
+
+const mapDispatch = {
+
+};
+
+export default connect(mapState, mapDispatch)(withStyles(s)(LeftNavigation));
